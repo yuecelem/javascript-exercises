0d196fc23707e84804f0503380154ee09bd55527
yuecelem <emre.ycl@hotmail.com>
2024-12-08 18:37:29 +0300

    Doenst work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let combinedString = [];
+    for (i = 0; i < num; i++) {
+        combinedString = combinedString.concat(string);
+    }
+    return combinedString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
