7d9625dc72fc005fef83a0e057a4835c9c3e13f7
yuecelem <emre.ycl@hotmail.com>
2024-12-09 22:14:14 +0300

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(nthNumber) {
 
+    if (nthNumber >= 2) {
+        let zeroNumber = 0;
+        let firstNumber = 1;
+        for (i = 1; i < nthNumber; i++) {
+        number = zeroNumber + firstNumber;
+        zeroNumber = firstNumber;
+        firstNumber = number;
+    } return number;
+    } else if (nthNumber == 0) return 0;
+    else if (nthNumber == 1) return 1;
+    else if (nthNumber < 0) return "OOPS";
 };
 
 // Do not edit below this line
