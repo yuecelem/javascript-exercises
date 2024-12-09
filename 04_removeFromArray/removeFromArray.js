bd941dd07a6f10897778b5ac69d7eccbeff0f277
yuecelem <emre.ycl@hotmail.com>
2024-12-09 19:07:53 +0300

    Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...toBeRemoved) {
+    let newArray = [];
+    array.forEach((item) => {
+        if(!toBeRemoved.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
