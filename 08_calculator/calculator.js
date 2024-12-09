3209f8e58c4d64b83db237dee82e4570c4de9249
yuecelem <emre.ycl@hotmail.com>
2024-12-09 21:12:47 +0300

    Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	addresult = number1 + number2;
+  return addresult;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	subresult = number1 - number2;
+  return subresult;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumresult = 0;
+  array.forEach((item) => {
+    sumresult += item;    
+  });  
+	return sumresult;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multresult = 1;
+  array.forEach((item) => {
+    multresult = multresult * item;
+  })
+  return multresult;
 };
 
-const power = function() {
-	
+const power = function(number, powernumber) {
+	powerresult = Math.pow(number, powernumber);
+  return powerresult;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factresult = 1;
+  for (i = 1; i <= number; i++)
+    factresult = factresult * i;
+  return factresult;
 };
 
 // Do not edit below this line
