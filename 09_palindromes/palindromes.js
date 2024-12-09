9481cea418c49250a16fdee85a3e6cd6335f51ba
yuecelem <emre.ycl@hotmail.com>
2024-12-09 21:35:28 +0300

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let noPun = string;
+    noPun = noPun.replace(/ /g, "");
+    noPun = noPun.replace(/,/g, "");
+    noPun = noPun.replace(/\./g, "");
+    noPun = noPun.replace(/!/g, "");
+    noPun = noPun.toLowerCase()
 
+    let reverseArray = [];
+    reverseArray = noPun.split("").reverse().join("")
+    
+    if (reverseArray == noPun) return true;
+    else return false;
+    
 };
 
 // Do not edit below this line
