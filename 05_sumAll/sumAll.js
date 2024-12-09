8d1b594b4ea38772c596d8d0787cbd663a4848db
yuecelem <emre.ycl@hotmail.com>
2024-12-09 19:55:46 +0300

    Donezo

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    let largerNumber = 0;
+    let smallerNumber = 0;
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    if (firstNumber % 1 != 0 || secondNumber % 1 != 0) {
+        return "ERROR";
+    }
+    if (typeof(firstNumber) != 'number' || typeof(secondNumber) != 'number') {
+        return "ERROR";
+    }
+    if (firstNumber > secondNumber){
+        largerNumber = firstNumber;
+        smallerNumber = secondNumber;
+    } else {
+        largerNumber = secondNumber;
+        smallerNumber = firstNumber;
+    }
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
