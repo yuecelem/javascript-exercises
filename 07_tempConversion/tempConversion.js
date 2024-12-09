98c428152ce8f6cec7c3520de351ba760635e786
yuecelem <emre.ycl@hotmail.com>
2024-12-09 21:00:15 +0300

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let converted = 0;
+  converted = ((fahrenheit - 32) * 5) / 9;
+  converted = Math.round((converted + Number.EPSILON) * 10) / 10
+  return converted
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let converted = 0;
+  converted = ((celsius * 9) / 5) + 32;
+  converted = Math.round((converted + Number.EPSILON) * 10) / 10
+  return converted
 };
 
 // Do not edit below this line
